a88ebd691f95a41a7e818f387117d8f6ae485211
O-Carneiro <ocarneiro1@usp.br>
2024-06-04 00:43:32 -0300

    criadir funcionando

--- EP3/ep3.c
+++ EP3/ep3.c
@@ -1,4 +1,5 @@
 #include "ep3.h"
+#include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,12 +9,12 @@
 int mountedFS = FALSE;
 FILE *FSFile;
 uint8_t *BMP;
-int *FAT;
+uint16_t *FAT;
 //FUNCOES MISC
 void loadBMP(){
     for(int i = 0; i < BITMAP_BYTES; i++){
         uint8_t byte;
-        fread(&byte, sizeof(uint8_t), 1, FSFile);
+        (void)!fread(&byte, sizeof(uint8_t), 1, FSFile);
         BMP[i] = byte;
     }
 }
@@ -24,10 +25,16 @@ void setBMP(uint8_t index, bool b){
         BMP[i] = BMP[i] | 128 >> index%8;
     else
         BMP[i] = BMP[i] & (0xFF - (128 >> index%8));
+    printf("BMP[%d] is now %02x\n", i, BMP[i]);
 }
 
 void loadFAT(){
-
+    FAT[0] = LAST_BLOCK;
+    for(int i = 1; i < 13; i++){
+        FAT[i] = i+1;
+    }
+    FAT[14] = LAST_BLOCK;
+    FAT[15] = LAST_BLOCK;
 }
 
 void createMountableFile(){
@@ -37,63 +44,154 @@ void createMountableFile(){
     uint8_t *data = calloc(DISK_BYTES, sizeof(uint8_t));
     data[0] = 0xFF; //11111111
     data[1] = 0xFE; //11111110
-    //todo: escrever o FAT inicial no arquivo.
+    //todo: write fat to disk;  
     fwrite(data, sizeof(uint8_t), DISK_BYTES, FSFile);
     free(data);
     rewind(FSFile);
     loadBMP();
     loadFAT();
+    //BIG TO DO: ONLY WRITE BMP AND FAT TO 
+    // DISK ONCE UMOUNTING / exiting;
 }
-uint16_t findFreeSpace(){
+uint16_t findFreeBlock(){
     for(int i = 0; i < BLOCK_NUMBER; i++){
         for(int j = 0; j < 8; j++){
-        if(!(BMP[i] & 128 >> j))
-            return (i*8) + j;
+            if(!(BMP[i] & 128 >> j)){
+                uint16_t ret = (i*8) + j;
+                FAT[ret] = LAST_BLOCK;
+                setBMP(ret, true);
+                return ret;
+            }
         }
     }
     printf("DISCO CHEIO, impossível continuar.");
     return -1;
 }
 
-
-//COMANDOS DO FS
-void criadir(char *dirName){
-    int stringLen = strlen(dirName);
-    //limite de tamanho para o tamanho da entrada caber em 8 bits.
-    if(stringLen > 239){
-        printf("criadir: o nome do diretorio deve ser menor.");
+void findEntrySpace(uint16_t size){
+    uint16_t curBlock = ftell(FSFile)/4096;
+    uint16_t blockPtr;
+    while(true){
+        (void)!fread(&blockPtr, 2, 1, FSFile);
+        fseek(FSFile, -2, SEEK_CUR);
+        blockPtr = htons(blockPtr);
+        //se nao ha mais entradas no bloco atual
+        if(blockPtr == 0){
+            //se cabe a entrada nesse bloco
+            if((curBlock +1)*4096 - ftell(FSFile) >= size){
+                return;
+            }
+            else{
+                //se for o ultimo bloco.
+                if(FAT[curBlock] == LAST_BLOCK){
+                        FAT[curBlock] = findFreeBlock();
+                        curBlock = FAT[curBlock];
+                        printf("Now going to block: %d\n", curBlock);
+                        fseek(FSFile, curBlock * BLOCK_SIZE, SEEK_SET);
+                        return;
+                }
+                //se nao for, continua buscando no proximo bloco.
+                else {
+                    curBlock = FAT[curBlock];
+                    fseek(FSFile, curBlock * BLOCK_SIZE, SEEK_SET);
+                    continue;
+                }
+            }
+        }
+        if(blockPtr & DIR_INDICATOR){
+            blockPtr -= DIR_INDICATOR;
+            fseek(FSFile, DIR_ATTR_SIZE+2, SEEK_CUR);
+        }
+        else
+            fseek(FSFile, FIL_ATTR_SIZE+2, SEEK_CUR);
+        uint8_t c;
+        while((c = fgetc(FSFile)) != '\0'){}
     }
-    //começa a busca no ROOT
-    fseek(FSFile, ROOT_ADDR, SEEK_SET);
-    //vai até o diretório certo e bota o ponteiro de leitura no lugar 
-    //certo.
-    //todo;
-    //O tamanho da entrada será:
-    // 1B = tamanho da entrada
-    // 2B = ponteiro pro bloco do diretorio
-    // 12B = 3 time_t pros instantes de tempo (serão apenas 4 bytes até 2038)
-    // nB = n é o tamanho da string de nome contando o 0 final.
-    // TOTAL = 15 + n B.
-    uint8_t size = 16 + stringLen;
-    uint16_t freeSpace = findFreeSpace();
-    FAT[freeSpace] = -1;
-    setBMP(freeSpace, true);
+    
+}
+
+void writeEntry(char *entryName, uint8_t ehDir){
+    int stringLen = strlen(entryName);
+    //limite de tamanho para o tamanho da entrada caber em 8 bits.
+    if(stringLen > 240){
+        printf("criadir: o nome do diretorio deve ser menor.");
+        return;
+    }
+    uint8_t size = 15 + stringLen;
+    findEntrySpace(size);
+    uint16_t freeBlock = findFreeBlock();
     time_t now = time(NULL);
     uint8_t *data = malloc(size);
-    data[0] = size;
-    data[1] = LAST((freeSpace + DIR_INDICATOR) >> 8,8);
-    data[2] = LAST(freeSpace,8);
+    data[0] = LAST((freeBlock + DIR_INDICATOR) >> 8,8);
+    data[1] = LAST(freeBlock,8);
     for(int j = 0; j < 3; j++){
         for(int i = 3; i >=0 ; i--){
-            data[6-i+(j*4)] = LAST(now >> i*8, 8);
+            data[5-i+(j*4)] = LAST(now >> i*8, 8);
         }
     }
-    fwrite(data, sizeof(uint8_t), 15, FSFile);
+    fwrite(data, sizeof(uint8_t), size - (stringLen + 1), FSFile);
     for(int i = 0; i < stringLen; i++){
-        fputc(dirName[i], FSFile);
+        fputc(entryName[i], FSFile);
     }
     fputc('\0', FSFile);
     free(data);
+
+}
+
+
+//COMANDOS DO FS
+void criadir(char *dirName){
+    char *nextDir = strtok(dirName, "/");
+    //começa a busca no ROOT
+    uint16_t curBlock = 14;
+    fseek(FSFile, ROOT_ADDR, SEEK_SET);
+    char *parent = dirName;
+    nextDir = strtok(NULL, "/");
+    //eh filho do ROOT
+    if(nextDir == NULL){
+        writeEntry(parent, DIR);
+        return;
+    }
+    //se nao eh filho do root
+    uint16_t blockPtr;
+    while(true){
+        (void)!fread(&blockPtr, 2, 1, FSFile);
+        blockPtr = htons(blockPtr);
+        //se nao ha mais entradas no bloco atual
+        if(blockPtr == 0){
+            //se for o ultimo bloco
+            if(FAT[curBlock] & LAST_BLOCK){
+                return;
+            }
+            else {
+                curBlock = FAT[curBlock];
+                fseek(FSFile, curBlock * BLOCK_SIZE, SEEK_SET);
+                continue;
+            }
+        }
+        if(blockPtr & DIR_INDICATOR){
+            blockPtr -= DIR_INDICATOR;
+            fseek(FSFile, DIR_ATTR_SIZE, SEEK_CUR);
+        }
+        else
+            fseek(FSFile, FIL_ATTR_SIZE, SEEK_CUR);
+        uint8_t i = 0, c;
+        char name[240];
+        while((c = fgetc(FSFile)) != '\0'){
+            name[i++] = c; 
+        }
+        name[i] = c;
+        if(strcmp(name, parent) == 0){
+            fseek(FSFile, blockPtr*BLOCK_SIZE, SEEK_SET);
+            parent = nextDir;
+            nextDir = strtok(NULL, "/");
+            if(nextDir == NULL){
+                writeEntry(parent, DIR);
+                return;
+            }
+        }
+    }
+
 }
 void monta(char *path){
     FSFile = fopen(path, "rb+");
--- EP3/ep3.h
+++ EP3/ep3.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,14 +9,19 @@
 #define TRUE 1
 #define FALSE 0
 #define BLOCK_NUMBER 25600
+#define BLOCK_SIZE 4096
 #define DISK_BYTES 104857600
 #define DISK_HEX_VALUES 52428800
 #define BITMAP_BYTES 3200
-#define BLOCK_SIZE 4096
 #define ROOT_ADDR 57344 
+#define ROOT_BLOCK 14 
 #define DIR_INDICATOR 32768
+#define LAST_BLOCK 32768
 #define LAST(k,n) ((k) & ((1<<(n))-1))
-#define MID(k,m,n) LAST((k)>>(m),((n)-(m)))
+#define DIR 1
+#define FIL 0
+#define DIR_ATTR_SIZE 12
+#define FIL_ATTR_SIZE 14
 
 char *promptUser();
 int handleCommand(char *command);
